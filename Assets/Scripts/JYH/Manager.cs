cdbbbc46f9f4e4504bebb82e89e296680e59a219
jungyounghan <superbrush@naver.com>
2025-05-08 14:58:07 +0900

    [Add] TextMeshPro 기능 및 폰트 추가
    추가된 폰트 이름: LiberationSans.ttf

---
+++ Assets/Scripts/JYH/Manager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// 각각의 씬 안에 유일한 객체로 존재하며 씬 안에 포함되는 모든 객체들을 하향식으로 통제함
+/// </summary>
+[DisallowMultipleComponent]
+public abstract class Manager : MonoBehaviour
+{
+    //씬 전환을 위한 별도의 컴포넌트가 필요함, 그 클래스가 있다면 그 클래스 객체를 멤버로 가지고 있어야 함(아마 로딩바를 포함한 ui이 패널이 될 듯함)
+
+#if UNITY_EDITOR
+    private static Manager _instance = null;
+
+    protected virtual void OnValidate()
+    {
+        if (gameObject.scene == SceneManager.GetActiveScene())
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<Manager>();
+            }
+            if (this == _instance)
+            {
+            }
+            UnityEditor.EditorApplication.delayCall += () =>
+            {
+                if (this != _instance && this != null)
+                {
+                    UnityEditor.Undo.DestroyObjectImmediate(gameObject);
+                }
+            };
+        }
+    }
+#endif
+
+    //
+    public abstract void Initialize();
+
+}
