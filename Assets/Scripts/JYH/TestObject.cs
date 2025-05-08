dbf01a414f15f46a6fc13ec985a8904ce9d4e2ee
hyugowacha <uena0506@gmail.com>
2025-05-08 18:20:27 +0900

    [FIx] 코드 임시 주석처리

--- Assets/Scripts/JYH/TestObject.cs
+++ Assets/Scripts/JYH/TestObject.cs
@@ -22,8 +22,8 @@ public class TestObject : MonoBehaviourPunCallbacks
         }
     }
 
-    public override void OnPlayerEnteredRoom(Player player)
-    {
+    //public override void OnPlayerEnteredRoom(Player player)
+    //{
 
-    }
+    //}
 }
