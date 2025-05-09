87778f2149f6f1c08f5b2da943edd5de0b7e8cc1
hyugowacha <uena0506@gmail.com>
2025-05-09 20:03:22 +0900

    [Add] 플레이어 상태 스크립트 추가 외
    인풋 시스템 추가

---
+++ Assets/Scripts/CEA/Player/PlayerStates/PickingState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickingState : IPlayerState
+{
+    public void EnterState(PlayerController player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void FixedUpdateState(PlayerController player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateState(PlayerController player)
+    {
+        throw new System.NotImplementedException();
+    }
+}
