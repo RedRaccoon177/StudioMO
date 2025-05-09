e81e4dd42b4a2db32745fed6a6b187963d0dff8a
Sonseongtae <sonseongtae@gmail.com>
2025-05-09 14:10:56 +0900

    [Feat] 채집물 스크립터블 오브젝트, 풀 기능 추가 ( 수정중 )

--- Assets/Scripts/SST/CollectionPool.cs
+++ Assets/Scripts/SST/CollectionPool.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class CollectionPool : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // ▼ 스크립터블 오브젝트 담을 리스트
+    [SerializeField] List<CollectionData> collectionDatas;
 
-    // Update is called once per frame
-    void Update()
+    // ▼ 각 채집물 이름별로 큐를 만들어 오브젝트를 딕셔너리에 저장
+    Dictionary<string, Queue<GameObject>> pools = new Dictionary<string, Queue<GameObject>>();
+
+    private void Awake()
     {
-        
+        Queue<GameObject> collectionDataQueue;
+
+        foreach (var collectionData in collectionDatas)
+        {
+            collectionDataQueue = new Queue<GameObject>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var obj = Instantiate(collectionData.collectionPrefab);
+                obj.SetActive(false);
+                obj.transform.parent = this.transform;
+                collectionDataQueue.Enqueue(obj);
+            }
+
+            pools.Add(collectionData.collectionName, collectionDataQueue);
+        }
     }
 }
