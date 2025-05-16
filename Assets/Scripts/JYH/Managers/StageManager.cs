92f1816e0b92cbddf4d21fcd89a7c2163c47498b
jungyounghan <superbrush@naver.com>
2025-05-16 17:59:28 +0900

    [WIP] 스테이지 씬 작업 시작

--- Assets/Scripts/JYH/Managers/StageManager.cs
+++ Assets/Scripts/JYH/Managers/StageManager.cs
@@ -1,12 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class StageManager : Manager
 {
     public static readonly string SceneName = "StageScene";
 
-    //[Header(nameof(StageManager))]
+    [Header(nameof(StageManager))]
+    [SerializeField, Range(0, int.MaxValue)]
+    private float remainingTime = 0.0f;
+
+    protected override void Update()
+    {
+        base.Update();
+        if(remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+
+        }
+    }
 
     protected override void Initialize()
     {
