94264aa82ce8cfbfec3170635ef32d0ed3718acd
jungyounghan <superbrush@naver.com>
2025-05-19 13:07:02 +0900

    [Add] 플레이어 모델링 추가

--- Assets/Scripts/CEA/Player/Player.cs
+++ Assets/Scripts/CEA/Player/Player.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class Player : MonoBehaviour
+[DisallowMultipleComponent]
+[RequireComponent(typeof(PhotonView))]
+public class Player : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
